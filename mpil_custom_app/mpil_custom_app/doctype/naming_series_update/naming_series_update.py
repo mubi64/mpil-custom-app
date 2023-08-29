9e9936dad91959f0f03324e8ce2b4589289336a9
Muhammad Saad <72243425+saadsafda@users.noreply.github.com>
2023-08-29 19:20:24 +0500

    Update naming_series_update.py

--- mpil_custom_app/mpil_custom_app/doctype/naming_series_update/naming_series_update.py
+++ mpil_custom_app/mpil_custom_app/doctype/naming_series_update/naming_series_update.py
@@ -15,10 +15,20 @@ class NamingSeriesUpdate(Document):
 			self.current_value = data[0][1]
 
 	def on_submit(self):
-		values = {'name': self.series, 'new_value': self.new_value}
-		frappe.db.sql("""
-			UPDATE `tabSeries` SET current=%(new_value)s where name=%(name)s
+		values = {'name': self.get("series")}
+		data = frappe.db.sql("""
+			select * from `tabSeries` where name=%(name)s
 		""", values=values, as_dict=0)
+
+		values = {'name': self.series, 'new_value': self.new_value}
+		if len(data)==0:
+			frappe.db.sql("""
+				INSERT INTO `tabSeries` (`name`, `current`) VALUES (%(name)s,%(new_value)s)
+			""", values=values, as_dict=0)
+		else:
+			frappe.db.sql("""
+				UPDATE `tabSeries` SET current=%(new_value)s where name=%(name)s
+			""", values=values, as_dict=0)
 
 @frappe.whitelist()
 def execute_query(doc):
